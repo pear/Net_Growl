4e6c2e4f69b9f29020506d2f97580ef896c65d56
Laurent Laville <farell@php.net>
2011-10-22 07:23:32 +0000

    Use PHPUnit/Autoload.php instead of requiring the files manually
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_Growl/trunk@318313 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -17,8 +17,7 @@ if (!defined('PHPUnit_MAIN_METHOD')) {
     define('PHPUnit_MAIN_METHOD', 'Net_Growl_AllTests::main');
 }
 
-require_once 'PHPUnit/Framework.php';
-require_once 'PHPUnit/TextUI/TestRunner.php';
+require_once 'PHPUnit/Autoload.php';
 
 require_once dirname(__FILE__) . '/Growl/ApplicationTest.php';
 require_once dirname(__FILE__) . '/Growl/GntpTest.php';
