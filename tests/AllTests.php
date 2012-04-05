9c4b96d80ba8324a9381aa3731d698e9f164727f
Laurent Laville <farell@php.net>
2012-04-05 10:10:31 +0000

    Complete All Tests suite launcher with specific features of version 2.6.0
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_Growl/trunk@324850 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -19,6 +19,7 @@ if (!defined('PHPUnit_MAIN_METHOD')) {
 
 require_once 'PHPUnit/Autoload.php';
 
+require_once dirname(__FILE__) . '/Growl/GrowlBaseTest.php';
 require_once dirname(__FILE__) . '/Growl/ApplicationTest.php';
 require_once dirname(__FILE__) . '/Growl/GntpTest.php';
 require_once dirname(__FILE__) . '/Growl/GntpMockTest.php';
@@ -36,6 +37,7 @@ class Net_Growl_AllTests
     public static function suite()
     {
         $suite = new PHPUnit_Framework_TestSuite('Net_Growl package');
+        $suite->addTestSuite('Net_Growl_GrowlBaseTest');
         $suite->addTestSuite('Net_Growl_ApplicationTest');
         $suite->addTestSuite('Net_Growl_GntpTest');
         $suite->addTestSuite('Net_Growl_GntpMockTest');
